92b946b4f02b656bce74ae453e2c771cd2db6c5a
Lucas Phelipe Castanho Ribeiro <135936579+LucasCastanh0@users.noreply.github.com>
2024-04-28 22:08:45 -0300

    Finish Commit

--- controle-fluxo/src/Contador.java
+++ controle-fluxo/src/Contador.java
@@ -4,8 +4,24 @@ public class Contador {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite o primeiro parametro: ");
-        sc.nextInt();
+        int parametroUm = sc.nextInt();
         System.out.println("Digite o segundo parametro: ");
-        sc.nextInt()
+        int parametroDois = sc.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        }catch (ParametrosInvalidosException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm >= parametroDois){
+            throw new ParametrosInvalidosException();
+        }
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++){
+            System.out.println("Número: " +i);
+        }
     }
 }
